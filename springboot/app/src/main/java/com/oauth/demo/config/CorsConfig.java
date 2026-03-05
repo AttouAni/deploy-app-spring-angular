2cde7eef104253635dbc75b986a08f2d3ee8d312
Anis AttouAni <149203786+AttouAni@users.noreply.github.com>
2026-03-05 08:58:12 +0000

    cors fix 3

--- springboot/app/src/main/java/com/oauth/demo/config/CorsConfig.java
+++ springboot/app/src/main/java/com/oauth/demo/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // Add your allowed origins here
+                .allowedOrigins("https://stunning-space-sniffle-q77qrj6v9rqq34p5w-4200.app.github.dev") // Add your allowed origins here
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
